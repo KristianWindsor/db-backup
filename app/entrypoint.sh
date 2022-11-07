1eb47874aec6d4205dcf6069d303ea1f75ae7c06
KristianWindsor <kristianwindso@gmail.com>
2022-11-06 16:49:07 -0800

    create backup

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "hello world"
+# create backup
+if [[ $DB_ENGINE == *"mysql"* ]]; then
+    export MYSQL_PWD="$DB_PASSWORD"
+    set -x
+    mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME.sql
+elif [[ $DB_ENGINE == *"postgres"* ]]; then
+    export PGPASSWORD="$DB_PASSWORD"
+    set -x
+    pg_dump -h $DB_HOST -p $DB_PORT -Fc -o -U $DB_USERNAME $DB_NAME > $FILE_NAME.dump
+fi
 
+# keep alive
 while true; do sleep 10000; done
