7916c8cfa706b48ed61894bd08056c13ec1ee8f0
KristianWindsor <kristianwindso@gmail.com>
2022-11-07 06:46:00 -0800

    add date to file name

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,17 +2,19 @@
 
 # create backup
 if [[ $DB_ENGINE == *"mysql"* ]]; then
+    FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').sql"
     export MYSQL_PWD="$DB_PASSWORD"
     set -x
-    mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME.sql
+    mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME
 elif [[ $DB_ENGINE == *"postgres"* ]]; then
+    FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').dump"
     export PGPASSWORD="$DB_PASSWORD"
     set -x
-    pg_dump -h $DB_HOST -p $DB_PORT -Fc -o -U $DB_USERNAME $DB_NAME > $FILE_NAME.dump
+    pg_dump -h $DB_HOST -p $DB_PORT -Fc -o -U $DB_USERNAME $DB_NAME > $FILE_NAME
 fi
 
 # upload to s3
-aws s3 cp $FILE_NAME* s3://$S3_BUCKET_NAME/
+aws s3 cp $FILE_NAME s3://$S3_BUCKET_NAME/
 
 # keep alive
 while true; do sleep 10000; done
