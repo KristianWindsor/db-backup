611f39793ac5eba4e133623dca6e42a90b0352e7
KristianWindsor <kristianwindso@gmail.com>
2022-11-07 13:05:42 -0800

    fixed mysqldump for older versions

--- app/backup.sh
+++ app/backup.sh
@@ -5,7 +5,7 @@ if [[ $DB_ENGINE == *"mysql"* ]]; then
     FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').sql"
     export MYSQL_PWD="$DB_PASSWORD"
     set -x
-    mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME
+    mysqldump --column-statistics=0 -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME
 elif [[ $DB_ENGINE == *"postgres"* ]]; then
     FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').dump"
     export PGPASSWORD="$DB_PASSWORD"
