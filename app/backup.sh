3cdf810365be4cb325059318f0d9e1fdb4c3bd69
KristianWindsor <kristianwindso@gmail.com>
2022-11-14 10:35:05 -0800

    verbose

--- app/backup.sh
+++ app/backup.sh
@@ -5,12 +5,12 @@ if [[ $DB_ENGINE == *"mysql"* ]]; then
     FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').sql"
     export MYSQL_PWD="$DB_PASSWORD"
     set -x
-    mysqldump --column-statistics=0 -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME
+    mysqldump --column-statistics=0 --verbose -h $DB_HOST -P $DB_PORT -u $DB_USERNAME $DB_NAME > $FILE_NAME
 elif [[ $DB_ENGINE == *"postgres"* ]]; then
     FILE_NAME="$FILE_NAME-$(date '+%Y-%m-%d').dump"
     export PGPASSWORD="$DB_PASSWORD"
     set -x
-    pg_dump -h $DB_HOST -p $DB_PORT -Fc -U $DB_USERNAME $DB_NAME > $FILE_NAME
+    pg_dump --verbose -h $DB_HOST -p $DB_PORT -Fc -U $DB_USERNAME $DB_NAME > $FILE_NAME
 fi
 
 # upload to s3
